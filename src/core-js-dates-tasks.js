3cd720aaee50f76b31bedc1de141906a6c3b4642
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2025-12-18 17:40:13 +0100

    feat: implement formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -158,8 +158,14 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(dateX) {
+  const date = new Date(dateX);
+  return `${date.getUTCMonth() + 1}/${date.getUTCDate()}/${date.getUTCFullYear()}, ${
+    date.getUTCHours() % 12 || 12
+  }:${date.getUTCMinutes().toString().padStart(2, '0')}:${date
+    .getUTCSeconds()
+    .toString()
+    .padStart(2, '0')} ${date.getUTCHours() >= 12 ? 'PM' : 'AM'}`;
 }
 
 /**
